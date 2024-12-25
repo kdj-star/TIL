00d21be0b5f57915e89ab107e797aaa085a9c683
kdj-star <kdj1994@kakao.com>
2024-12-25 21:06:16 +0900

    key.txt 저장위치 수정

--- pybithumb_project/_pybithumb.py
+++ pybithumb_project/_pybithumb.py
@@ -15,11 +15,13 @@ import webbrowser
 import sys
 from PyQt5 import uic
 from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 import time
 import pybithumb
 
-f = open("./key.txt", 'r')
+
+f = open("D:/key.txt", 'r')
 lines = f.readlines()
 
 con_key = lines[0].split('\n')[0]
@@ -28,7 +30,7 @@ sec_key = lines[1]
 print(sec_key)
 bithumb = pybithumb.Bithumb(con_key, sec_key)
 
-form_class = uic.loadUiType("./241221.ui")[0]
+form_class = uic.loadUiType("D:/241221.ui")[0]
 def rsi(name,time):
     now = datetime.now()
 
@@ -82,6 +84,9 @@ class WindowClass(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
         self.timer = QtCore.QTimer()
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.buy.clicked.connect(self._buy)
+        self.sell.clicked.connect(self._sell)
         self.timer.timeout.connect(self.setTableWidgetData)
         self.timer.start(1000)
         #self.pushButton.clicked.connect(_print)
@@ -89,18 +94,73 @@ class WindowClass(QMainWindow, form_class):
 
     def setTableWidgetData(self):
           ticker = self.comboBox.currentText()
-          tmp = pybithumb.get_ohlcv()
+          detail = bithumb.get_market_detail(ticker)
           price_ticker = round(pybithumb.get_current_price(ticker),4)
           balance = bithumb.get_balance(ticker)
           total = round(balance[0] * pybithumb.get_current_price(ticker),1)
-          self.lineEdit_6.setText(str(price_ticker))
-          self.lineEdit_7.setText(str(total))
-          self.lineEdit_2.setText(str(rsi(ticker,'10m')))
-          self.lineEdit_3.setText(str(rsi(ticker,'30m')))
-          self.lineEdit_4.setText(str(rsi(ticker,'1h')))
-          self.lineEdit_5.setText(str(rsi(ticker,'4h')))
-        
+          self.price.setText(str(price_ticker))
+          self.total.setText(str(total))
+          self.rsi_10m.setText(str(rsi(ticker,'10m')))
+          self.rsi_30m.setText(str(rsi(ticker,'30m')))
+          self.rsi_1h.setText(str(rsi(ticker,'1h')))
+          self.rsi_4h.setText(str(rsi(ticker,'4h')))
+          self.end_price.setText(str(round(detail[0],4)))
+          self.highest.setText(str(round(detail[1],4)))
+          self.lowest.setText(str(round(detail[2],4)))
+          self.end_price_2.setText(str(round(price_ticker-detail[0],4)))
+          self.highest_2.setText(str(round(price_ticker-detail[1],4)))
+          self.lowest_2.setText(str(round(price_ticker-detail[2],4)))
 
+    def _buy(self):
+        
+        buttonReply = QMessageBox.information(
+        self, 'Information Title', "Information Message", 
+        QMessageBox.Yes| QMessageBox.No, 
+        QMessageBox.No
+        )
+
+        if buttonReply == QMessageBox.Yes:
+            while True :
+                try:
+                    print('buy')
+                    ticker = self.comboBox.currentText()
+                    detail = bithumb.get_market_detail(ticker)
+                    price_ticker = round(pybithumb.get_current_price(ticker),4)
+                    balance = bithumb.get_balance('btc')[2] 
+                    bithumb.buy_market_order(ticker,int(balance/price_ticker*0.99))
+                    break
+                except:
+                    pass
+        else:
+            print('No clicked.')
+            
+        
+       
+    
+    def _sell(self):
+     buttonReply = QMessageBox.information(
+        self, 'Information Title', "Information Message", 
+        QMessageBox.Yes| QMessageBox.No, 
+        QMessageBox.No
+        )
+        
+        
+     if buttonReply == QMessageBox.Yes:
+        while True :
+            try:
+                ticker = self.comboBox.currentText()
+                detail = bithumb.get_market_detail(ticker)
+                price_ticker = round(pybithumb.get_current_price(ticker),4)
+                balance = bithumb.get_balance(ticker)
+                total = round(balance[0] * pybithumb.get_current_price(ticker),1)
+                print('sell')
+                bithumb.sell_market_order(ticker,int(balance[0]))
+                break
+            except:
+                pass
+     else:
+        print('No clicked.')
+        
 
 
     
